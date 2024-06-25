b3b553707d932fe39fc9a78a13fbe0c5f2677100
Johannes Bauer <joe@johannes-bauer.com>
2024-06-25 20:16:13 +0200

    Add LICENSE and license headers
    Forgot to include the license (GPL-3). Added now.

--- pyexamgrading/MoodleCSV.py
+++ pyexamgrading/MoodleCSV.py
@@ -1,20 +1,20 @@
-#	moodletools - Collection of scripts that are boilerplate between backend and MOODLE
-#	Copyright (C) 2022-2024 Johannes Bauer
+#	pyexamgrading - Manage grade computation of university exams
+#	Copyright (C) 2024-2024 Johannes Bauer
 #
-#	This file is part of moodletools.
+#	This file is part of pyexamgrading.
 #
-#	moodletools is free software; you can redistribute it and/or modify
+#	pyexamgrading is free software; you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation; this program is ONLY licensed under
 #	version 3 of the License, later versions are explicitly excluded.
 #
-#	moodletools is distributed in the hope that it will be useful,
+#	pyexamgrading is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
 #
 #	You should have received a copy of the GNU General Public License
-#	along with moodletools; if not, write to the Free Software
+#	along with pyexamgrading; if not, write to the Free Software
 #	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #	Johannes Bauer <JohannesBauer@gmx.de>
